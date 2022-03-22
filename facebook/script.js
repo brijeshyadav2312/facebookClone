4806d803f2e6f19a848ffdf0adac6deb6da754b4
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-03-22 17:58:43 +0530

    Add files via upload

---
+++ facebook/script.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    var x = document.getElementById("chatbox");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction1() {
+    var y = document.getElementById("chatbox");
+      y.style.display = "none";
+  }
+
+  function myFunction1() {
+    var x = document.getElementById("chatbox2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("post_popup");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
